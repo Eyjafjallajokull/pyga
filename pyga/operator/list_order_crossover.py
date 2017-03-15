8a42a391a9be4ee19ca0902a4d815d74c64dc051
Pawel Olejniczak <pawel.olejniczak@gmail.com>
2017-03-15 15:34:17 +0700

    Add ListOrderCrossoverOperation

---
+++ pyga/operator/list_order_crossover.py
@@ -0,0 +1,60 @@
+from .list_crossover import ListCrossoverOperator
+from copy import copy
+
+
+class ListOrderCrossoverOperator(ListCrossoverOperator):
+    """
+    Partially Mapped Cross-over (PMX) algorithm
+
+    Designed for lists of unique items for which order is important.
+
+    :param probability: Probability
+    :param random: Random
+    """
+    def __init__(self, probability, random):
+        super().__init__(2, probability, random)
+
+    def mate_lists(self, parent_list1, parent_list2):
+        """
+        Crossover two lists.
+
+        :param parent_list1: list
+        :param parent_list2: list
+        :return: list, list
+        """
+        indexes = [self.random.int(0, len(parent_list1)-1) for _ in range(self.crossover_points)]
+        indexes.sort()
+        list1 = copy(parent_list1)
+        list2 = copy(parent_list2)
+        for j in range(indexes[0], indexes[1]):
+            tmp = list1[j]
+            list1[j] = list2[j]
+            list2[j] = tmp
+        list1 = self.legalize_offspring_list(list1, parent_list1, indexes)
+        list2 = self.legalize_offspring_list(list2, parent_list2, indexes)
+        return list1, list2
+
+    def legalize_offspring_list(self, subject, other, segment):
+        """
+        After initial crossover, lists may not contain all items, and some
+        will be repeated. This method fixes subject list.
+
+        :param subject: list
+        :param other: list
+        :param segment: list
+        :return: list
+        """
+        s0 = segment[0]
+        s1 = segment[1]
+        for n in range(s0, s1):
+            if other[n] not in subject[s0:s1]:
+                i = other[n]
+                j = subject[n]
+                index_of_j_in_other = other.index(j)
+                if index_of_j_in_other < s0 or index_of_j_in_other >= s1:
+                    subject[index_of_j_in_other] = i
+                else:
+                    k = subject[index_of_j_in_other]
+                    index_of_k_in_other = other.index(k)
+                    subject[index_of_k_in_other] = i
+        return subject
---
+++ tests/test_operator/test_list_order_crossover_operator.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from pyga import ValidationException
+from pyga.common import Probability, Random
+from pyga.candidate import Candidate
+from pyga.operator.list_order_crossover import ListOrderCrossoverOperator
+
+
+class ListCrossoverOperatorTestCase(TestCase):
+    def test_apply(self):
+        candidate1 = Candidate()
+        candidate1.data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        candidate2 = Candidate()
+        candidate2.data = [9, 3, 7, 8, 2, 6, 5, 1, 4]
+        random = Random()
+        random.int = MagicMock(side_effect=[3, 7])
+        crossover_operator = ListOrderCrossoverOperator(Probability(1), random)
+        result = crossover_operator.mate(candidate1, candidate2)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].data, [1, 7, 3, 8, 2, 6, 5, 4, 9])
+        self.assertEqual(result[1].data, [9, 3, 2, 4, 5, 6, 7, 1, 8])
+
+    def test_validate(self):
+        crossover_operator = ListOrderCrossoverOperator(Probability(1), Random())
+        candidate1 = Candidate()
+        candidate1.data = [1, 2, 3, 4]
+        candidate2 = Candidate()
+        candidate2.data = [5, 6, 7, 8]
+        crossover_operator.validate_parents(candidate1, candidate2)
+
+        candidate2.data = [1, 2, 3, 4, 5]
+        with self.assertRaises(ValidationException):
+            crossover_operator.validate_parents(candidate1, candidate2)
